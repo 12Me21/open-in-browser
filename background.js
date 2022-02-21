ee35240a961d87f659f9dc7c616fe800aadea01f
12Me21 <12Me21.mc@gmail.com>
2022-02-21 00:40:19 -0500

    fix

--- background.js
+++ background.js
@@ -2,15 +2,15 @@
 chrome.webRequest.onHeadersReceived.addListener(
 	(details)=>{
 		let changed = false
-		for (let header of headers)
+		for (let header of details.responseHeaders)
 			if (/^content-disposition$/i.test(header.name))
 				if (/^\s*attachment/i.test(header.value)) {
 					header.value = header.value.replace(/attachment/i, "inline")
 					changed = true
-					console.log("changed header!", headers)
+					//console.log("changed header!", details.responseHeaders)
 				}
 		if (changed)
-			return {responseHeaders: headers}
+			return {responseHeaders: details.responseHeaders}
 	},
 	{urls: ["<all_urls>"], types: ["main_frame","sub_frame"]},
 	["blocking", "responseHeaders"],
